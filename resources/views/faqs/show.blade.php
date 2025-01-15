989cf7926de7477c4f5963915a4772fe20e39742
Arurwe <artur.wlezien@gmail.com>
2025-01-15 12:49:16 +0100

    END pop

--- resources/views/faqs/show.blade.php
+++ resources/views/faqs/show.blade.php
@@ -1,39 +1,3 @@
-{{-- @extends('layouts.app')
-
-@section('content')
-    <div class="container mx-auto py-6 px-4">
-        <!-- Nagłówek tytułu FAQ -->
-        <h1 class="text-3xl font-bold text-gray-900 mb-4">{{ $faq->title }}</h1>
-        
-        <!-- Treść FAQ -->
-        <div class="bg-white shadow-md rounded-lg p-6 mb-6">
-            {!! $faq->content !!}
-           
-            
-        </div>
-
-        <!-- Informacje o kategorii -->
-        <div class="flex items-center space-x-4 mb-4">
-            <p class="text-sm text-gray-600"><strong>Kategoria:</strong><a class="hover:text-blue-500"href="{{ route('category.show',$faq->category) }}"> {{ $faq->category->name }}</a></p>
-        </div>
-
-        <!-- Tagi -->
-        @if($faq->tags->count() > 0)
-            <div class="flex flex-wrap items-center space-x-3">
-                <strong class="text-sm text-gray-600">Tagi:</strong>
-                @foreach($faq->tags as $tag)
-                    <span class="bg-blue-100 text-blue-800 text-xs font-medium px-2 py-1 rounded-full">{{ $tag->name }}</span>
-                @endforeach
-            </div>
-        @else
-            <p class="text-sm text-gray-600 mt-2">Brak tagów.</p>
-        @endif
-
-        {{-- Wyświetlenia --}}
-        {{-- <p class="text-sm text-gray-600 mt-2">Ilość wyświetleń: {{ $faq->views }}</p>
-    </div>
-@endsection
- --}} 
 
 
  @extends('layouts.app')
@@ -49,7 +13,10 @@
          <div class="bg-white shadow-md rounded-lg p-6 mb-6">
              {!! $faq->content !!}
          </div>
-         @if($faq->file_option ===1)
+    @elseif($faq->file_option ===1)
+    <div class="bg-white shadow-md rounded-lg p-6 mb-6">
+        {!! $faq->content !!}
+    </div>
          @foreach ($faq->files as $file)
          <div class="mb-4">
             <p>{{ $file->content_before }} <a href="{{ asset('storage/' . $file->file_path) }}" 
@@ -58,14 +25,12 @@
             </p>
         </div>
          @endforeach
-         @endif
-     @endif
+    
  
      <!-- Pliki -->
      
-         @if($faq->file_option === 3)
+         @elseif($faq->file_option === 3)
          @foreach ($faq->files as $file)
-12212
              <!-- Opcja 3: Wyświetlanie pliku na stronie -->
              <div class="mb-4">
                  <p>{{ $file->content_before }}</p>
